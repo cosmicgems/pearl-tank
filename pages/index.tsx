0f9915b03a6ef8b7eba61cdf692208a8e8e5fb19
Maliek Davis <maliekjdavis24@gmail.com>
2023-02-08 09:36:00 -0500

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -74,19 +74,6 @@ export default function Home() {
             </p>
           </a>
 
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
 
           <a
             href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
